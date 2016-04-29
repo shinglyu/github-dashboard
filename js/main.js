496f258f826e6d0084858fc4b2d70fba7835ab76
chatea <cha122977@gmail.com>
2016-04-29 21:26:16 +0800

    Adding hjkl movement and enter
    Note:
    Currently, the j/k movement is hard-coded
    as 3 (The column number).

--- js/main.js
+++ js/main.js
@@ -18,18 +18,19 @@ var Repo =  React.createClass({
     if (description != null && description.length > maxLength) {
       description = description.slice(0, maxLength) + "......"
     }
+    let cardClasses = "mui-col-md-4 repo-card" + (this.props.selected?" selected-card":"")
 
     return (
-      h('div', {className:"mui-col-md-4 repo-card"}, 
-        h('div', {className:"mui-panel", onClick:this.handleClick}, 
-          h('a', {href: info.html_url}, 
+      h('div', {className:cardClasses},
+        h('div', {className:"mui-panel", onClick:this.handleClick},
+          h('a', {href: info.html_url},
             h('h3', null, info.name)
           ),
-          h('p', null, 
+          h('p', null,
             h('a', {href:info.owner.html_url}, info.owner.login)
           ),
           h('p', {className:"description"}, description),
-          h('p', null, 
+          h('p', null,
             h('a', {href: info.html_url + "/issues"}, "Issues"),
             h('span', null,  " | "),
             h('a', {href: info.html_url + "/pulls"}, "PRs"),
@@ -42,9 +43,13 @@ var Repo =  React.createClass({
   }
 })
 var RepoList =  React.createClass({
+  openSelection: function(){
+    window.location.href = this.props.repos[this.props.selection].html_url
+  },
   render: function(){
+    let selection = this.props.selection;
     var lis = this.props.repos.map(function(repoinfo, idx){
-      return h(Repo, { key:idx, repoinfo: repoinfo} )
+      return h(Repo, { key:idx, repoinfo: repoinfo, selected:(idx==selection)?true:false} )
     })
     return (
       h('div', {className:"mui-row"}, lis)
@@ -54,11 +59,14 @@ var RepoList =  React.createClass({
 
 var Dashboard =  React.createClass({
   getInitialState: function() {
-    return {repos: [{name:"Loading...", owner:{}, description:''}]}
+    return {
+      repos: [{name:"Loading...", owner:{}, description:''}],
+      selection: -1,
+    }
   },
   componentDidMount: function() {
 
-    var re = /\?user=([\w-]*)/g; 
+    var re = /\?user=([\w-]*)/g;
     var matches = re.exec(location.search);
     var username = null;
     if (matches !== null && matches.length == 2) {
@@ -87,6 +95,7 @@ var Dashboard =  React.createClass({
           return result.json();
       })
       .then(function(resultjson){
+        this.setState({repoCount: resultjson.length})
         for (var idx in resultjson){
           var org = resultjson[idx]
           fetch(org.repos_url + repoParams)
@@ -105,13 +114,54 @@ var Dashboard =  React.createClass({
         }
       }.bind(this))
   },
+  inputKeyEvent: function(e){
+    let max = this.state.repos.length - 1
+    let move = function(current,diff){
+      if (0 > current){
+        return 0; // initial position
+      }
+      let update = current + diff
+      return update < 0? 0 : (update < max? update : max)
+    }
+    switch (e.which || e.keycode){
+      case 72: // h
+        this.setState(function(ps,cp){
+          return {selection: move(ps.selection,-1)}
+        })
+        break;
+      case 74: // j
+        this.setState(function(ps,cp){
+          return {selection: move(ps.selection,3)}
+        })
+        break;
+      case 75: // k
+        this.setState(function(ps,cp){
+          return {selection: move(ps.selection,-3)}
+        })
+        break;
+      case 76: // l
+        this.setState(function(ps,cp){
+          return {selection: move(ps.selection,1)}
+        })
+        break;
+      case 13: // enter
+        // TODO
+        this.refs.repolist.openSelection()
+        break;
+    }
+  },
   render: function(){
     return (
-      h('div', {className:"mui-row"}, 
+      h('div', {className:"mui-row"},
         h('h1', {className:"mui-col-md-12"}, 'My GitHub'),
-        h(RepoList, {repos:this.state.repos})
-       )
+        h(RepoList, {repos:this.state.repos, selection:this.state.selection, ref:"repolist"})
+      )
     )
   }
 })
-ReactDOM.render(React.createElement(Dashboard), document.getElementById('content'))
+
+var dashboard = ReactDOM.render(h(Dashboard), document.getElementById('content'))
+
+document.addEventListener("keydown", function(e){
+  dashboard.inputKeyEvent(e)
+})
